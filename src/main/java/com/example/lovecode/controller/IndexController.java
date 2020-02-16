b5175ce9108a77a2e0cd2b227a46a3bfc1e40238
cuiyaocy <cuiyaovip@126.com>
2020-02-16 14:13:47 +0800

    debug

--- src/main/java/com/example/lovecode/controller/IndexController.java
+++ src/main/java/com/example/lovecode/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.example.lovecode.controller;
 
 import com.example.lovecode.common.Constants;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author cuiyaocy
+ */
+@Api(tags = "Index接口")
 @RestController
 @RequestMapping("/api/index")
 public class IndexController {
+
     @GetMapping("/status")
     public String index() {
         return Constants.INDEX_STATUS;
--- src/main/java/com/example/lovecode/controller/LoginController.java
+++ src/main/java/com/example/lovecode/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.lovecode.controller;
 
 import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class LoginController {
             @ApiImplicitParam(value = "用户名", name = "name", required = true, dataType = "String", defaultValue = "null", paramType = "query"),
             @ApiImplicitParam(value = "密码", name = "pw", required = true, dataType = "String", defaultValue = "null", paramType = "query")}
     )
-    @RequestMapping("/bypw")
+    @PostMapping("/bypw")
     public Object loginByPw(@RequestParam String name, @RequestParam String pw) {
         return null;
     }
