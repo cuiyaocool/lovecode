27f6436223bc3cc02c1c556dc7fe9c2468b68939
cuiyaocool <cuiyaovip@126.com>
2020-04-26 18:54:40 +0800

    debug

--- src/main/java/com/example/lovecode/Aspect/TestAspect.java
+++ src/main/java/com/example/lovecode/Aspect/TestAspect.java
@@ -31,5 +31,5 @@ public class TestAspect {
         System.out.println(userEntity.getName());
     }
 
-    
+
 }
---
+++ src/test/java/com/example/lovecode/MapTest.java
@@ -0,0 +1,107 @@
+package com.example.lovecode;
+
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MapTest {
+
+    private Map<String, String> map;
+
+    @BeforeClass
+    public void init () {
+        System.out.println("beforeClass");
+        map = new HashMap() {{
+                    put("1", "a");
+                    put("2", "d");
+                    put("3", "F");
+                }};
+    }
+
+    @Before
+    public void before() {
+        System.out.println("before");
+        map = new HashMap() {{
+            put("1", "a");
+            put("2", "d");
+            put("3", "F");
+        }};
+    }
+
+    @Test
+    public void testAll() {
+        map = new HashMap() {{
+            put("1", "a");
+            put("2", "d");
+            put("3", "F");
+        }};
+        if (map == null) {
+            System.out.println("null");
+        }
+        assertTrue(map.containsValue("a"));
+        assertFalse(map.containsKey("r"));
+        map.forEach(new BiConsumer<String, String>() {
+            @Override
+            public void accept(String s, String s2) {
+                s2 = s2.toUpperCase();
+                map.put(s, s2);
+            }
+        });
+        System.out.println(map);
+        map.computeIfAbsent("4", new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s + "gg";
+            }
+        });
+        System.out.println(map);
+        map.computeIfAbsent("1", new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s + "gg";
+            }
+        });
+        System.out.println(map);
+        map.merge("4", "K", new BiFunction<String, String, String>() {
+            @Override
+            public String apply(String s, String s2) {
+                return "L";
+            }
+        });
+        System.out.println(map);
+        map.compute("4", new BiFunction<String, String, String>() {
+            @Override
+            public String apply(String s, String s2) {
+                return null;
+            }
+        });
+        System.out.println(map);
+        BiFunction<String, String, String> myBiFun = new BiFunction<String, String, String>() {
+            @Override
+            public String apply(String s, String s2) {
+                return "P";
+            }
+        };
+        map.compute("4", myBiFun);
+        System.out.println(map);
+        BiFunction<String, String, String> stringStringStringBiFunction = myBiFun.andThen((key) -> {
+            System.out.println(key + "ll");
+            return "ll" + key + "1";
+        });
+        map.compute("4", stringStringStringBiFunction);
+        System.out.println(map);
+
+
+    }
+
+}
